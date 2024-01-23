dc8a76e7ab183a614d5ec1f542cc96ec96f3da9c
nirnay01 <ravalnirnay@gmail.com>
2024-01-23 13:35:57 +0530

    try 05

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/", (req, res) => {
   const payload = req.body; // Access parsed body with req.body
   const temp=payload.ref.toString().split('/');
   console.log(temp[2]);
-  const temp1=payload.commits.message.toString();
+  const temp1=payload.commits;
   console.log("msg:",temp1);
   // Your logic to handle GitHub webhook events goes here
   // console.log("Received GitHub webhook payload:", payload);
