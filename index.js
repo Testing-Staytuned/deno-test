fe7c303c84760864ff04f3f9ce2ff92238484683
nirnay01 <ravalnirnay@gmail.com>
2024-01-23 13:34:22 +0530

    try 05

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(json()); // Use Opine's json middleware
 // Respond to POST requests to the "/webhook" endpoint (GitHub webhook events)
 app.post("/", (req, res) => {
   const payload = req.body; // Access parsed body with req.body
-  const temp=payload.ref.toString();
-  const temp1=temp.split('/')
-  console.log(temp1[2]);
-  
+  const temp=payload.ref.toString().split('/');
+  console.log(temp[2]);
+  const temp1=payload.commits.message.toString();
+  console.log("msg:",temp1);
   // Your logic to handle GitHub webhook events goes here
   // console.log("Received GitHub webhook payload:", payload);
 
